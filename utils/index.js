708f37d99ee171bac3ee49e80afa4190a17fdbce
KobzarAnatolii <TohaPirat@gmail.com>
2023-06-02 14:58:52 +0300

    add util sendEmail

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,11 @@
 const HttpError = require('./createError404');
 const handelMongooseError = require('./handelMongooseError');
 const controllerWrapper = require('./controllerWrapper');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
   HttpError,
   handelMongooseError,
   controllerWrapper,
+  sendEmail,
 };
